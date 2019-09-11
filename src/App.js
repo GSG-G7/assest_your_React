c2b7aa900e70a9d8078ac1b574aa752e6975cbde
Asmaathabet <asmathabet2009@hotmail.com>
2019-09-11 11:31:18 +0300

    create html,css, class Components,Function Components relates #7

--- src/App.js
+++ src/App.js
@@ -1,24 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
+import Card from './ClassComponents/Card';
+import Headers from './ClassComponents/Header';
+import request from './FunctionalComponents/request';
+import audio from './FunctionalComponents/audio';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     
     </div>
   );
 }
